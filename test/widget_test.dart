5eee69a9fca8ff8eec143eb672d3e55442e1bae0
lorenzoromandini <lorenzoromandini@live.it>
2026-03-09 06:36:17 +0100

    fix(01-foundation-auth-01): remove unused import in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,5 @@
 // This is a basic Flutter widget test for the Calcetto app.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:calcetto_app/core/app/app.dart';
 
